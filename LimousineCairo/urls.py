73ca2ba70c5bb33e60f19b34f77ba8bc0010642b
AbdelrhmanHamedTawfik <abdohamed391811@gmail.com>
2023-04-26 15:38:00 +0200

    Adjust project name

--- LimousineCairo/urls.py
+++ LimousineCairo/urls.py
@@ -1,4 +1,4 @@
-"""LimousinCairo URL Configuration
+"""LimousineCairo URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
-from LimousinCairo import settings
+from LimousineCairo import settings
 from django.contrib.sitemaps.views import sitemap
 from lc_app.sitemaps import *
 
